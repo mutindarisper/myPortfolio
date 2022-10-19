2a42149c216dea477c72a828e0a454049045b1b1
unknown <mutindarisper@gmail.com>
2022-10-19 12:37:48 +0300

    added linkedin, github twitter and instagram links to the icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import { BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebookF} from 'react-icons/fa'
+// import { FaFacebookF} from 'react-icons/gi'
+import {  AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 
 const SocialMedia = () => {
+
   return (
     <div className='app__social'> 
-        <div >
+        <div > 
+          <a href="https://twitter.com/MutukuRisper" target="_blank" rel="noopener noreferrer">
             <BsTwitter />
+            </a>
         </div>
         <div >
+          <a href="https://www.instagram.com/thesmartdev/" target="_blank" rel="noopener noreferrer">
             <BsInstagram />
+            </a>
         </div>
-        <div >
-            <FaFacebookF />
+        <div  >
+          <a href="https://github.com/mutindarisper" target="_blank" rel="noopener noreferrer">
+            < AiFillGithub  />
+            </a>
+        </div>
+        <div  >
+          <a href="https://www.linkedin.com/in/risper-mutuku-1b5647154/" target="_blank" rel="noopener noreferrer">
+            < AiFillLinkedin  />
+            </a>
         </div>
     </div>
   )
